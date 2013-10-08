d78f3cf7e8ab3e911dc5d8dfe0c7b07b8a9ab87f
moorehea1 <moorehea1@cs.grinnell.edu>
2013-10-07 22:01:46 -0500

    working

--- src/edu/grinnell/CSC207/moorehea1/hw6/dutchFlag.java
+++ src/edu/grinnell/CSC207/moorehea1/hw6/dutchFlag.java
@@ -5,54 +5,51 @@ import java.io.PrintWriter;
 public class dutchFlag {
 
     public static void swap(String[] array, int first, int second) {
-	String holder;
-
-	holder = array[first];
+	String holder = array[first];
 	array[first] = array[second];
 	array[second] = holder;
-
-    } // swap
+    } // swap(String[], int, int)
 
     public static void main(String[] args) throws Exception {
 	PrintWriter pen = new PrintWriter(System.out, true);
-
 	int R = 0;
-	int W = 1;
-	int B = 2;
-	int index = 3;
+	int W = 0;
+	int B = 0;
 	// String[] flag = new String[size];
-	String[] flag = { "blue", "white", "red", "red", "blue", "red", "white" };
+	String[] flag = {"blue", "red", "red", "white", "red", "white", "blue"/*, "white", "white", "blue", "white"*/};
 	int size = flag.length;
 
 	while (R <= W && W <= B && B < size) {
-	    
-	    switch (flag[index]) {
+	    switch (flag[B]) {
 	    case "red":
-		swap(flag, R, index);
+		swap(flag, R, B);
 		R++;
-		index++;
-		//W++;
-		//B++;
+		if (R > W)
+		    W++;
+		if (W > B)
+		    B++;
 		pen.println("test1");
 		break;
 	    case "white":
-		swap(flag, W, index);
+		swap(flag, W, B);
 		W++;
-		index++;
-		//B++;
+		if (W > B)
+		    B++;
 		pen.println("test2");
 		break;
 	    case "blue":
-		swap(flag, B, index);
 		B++;
-		index++;
 		pen.println("test3");
 		break;
 	    } // switch
 	} // while
-
+	pen.print("{");
 	for (int i = 0; i < size; i++) {
-	    pen.println(flag[i]);
-	}
+	    pen.print("\"" + flag[i] + "\"");
+	    if (i != size-1) {
+		pen.print(", ");
+	    }
+	} // for
+	pen.println("}");
     } // main
 } // dutchFlag
